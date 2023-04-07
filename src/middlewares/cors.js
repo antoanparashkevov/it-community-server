536c8bfe1067e2aff3741f0487099642b46a73bc
“antoanparashkevov” <tonkata1505@gmail.com>
2023-04-07 13:15:52 +0300

    fix

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = () => (req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://itcommunitys.vercel.app')
+    res.setHeader('Access-Control-Allow-Origin', 'https://itcommunity.vercel.app')
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,HEAD,GET,POST,PUT,DELETE,PATCH')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization')
     res.setHeader('Access-Control-Allow-Credentials', 'true')
