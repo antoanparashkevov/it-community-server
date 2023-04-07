8624fe4c357caf88fba9a838e88c2a57c74a2da4
“antoanparashkevov” <tonkata1505@gmail.com>
2023-04-07 10:21:05 +0300

    update the cors policy

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = () => (req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,HEAD,GET,POST,PUT,DELETE,PATCH')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization')
     res.setHeader('Access-Control-Allow-Credentials', 'true')
